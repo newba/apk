4bde6daf0083c8038ac05f0a1a333de635b88308
Marcelo Santos <msantosecnm@hotmail.com>
2017-09-26 11:54:50 -0400

    Comments
    Comments added in the whole code

--- TicTacToe/app/src/main/java/br/com/darksite/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/br/com/darksite/tictactoe/MainActivity.java
@@ -15,13 +15,17 @@ public class MainActivity extends AppCompatActivity {
     //Const de la tag de chaque button
     //j'utilise ce const pour recuperer le button utilisant le methode getCarre
         private final String CARRE = "carre";
+		
     // const que sera imprimé no text du button
         private final String BULE = "O";
+		
     // const que sera imprimé no text du button
         private final String XIS = "X";
+		
+	// ici on garde le dernier valeur du jeu	
         private String lastPlay = "X";
 
-        //Matriz(pt-br) pour les conditions de victoire
+        //Matrice que define les conditions de victoire
         int [][] etatFinal = new int[][]{
 
                 //Horizontal: si les buttons contient le meme text, le jeu est fini
@@ -43,38 +47,45 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+			
+			//ici on recupere le XML et on garde l'instance de VIEW utilisant le setView
 
             setView(getLayoutInflater().inflate(R.layout.activity_main, null));
+			
+			//ici on passe l'instance de notre VIEW à notre Activity
             setContentView(getView());
         }
 
         public void clickCarre (View v) {
 
-            if (!isEnd()) {
+            if (!isEnd()) { // on vérifie si le jeu est fini
 
-                if (((Button) v).getText().equals("")) {
-
-                    if (getLastPlay().equals(XIS)) {
-                        ((Button) v).setText(BULE);
-                        setLastPlay(BULE);
+                if (((Button) v).getText().equals("")) { // vérifie si le text du button était venu de la variable v
+				// est different de ""(vide)
+                    if (getLastPlay().equals(XIS)) {//vérifie si la dernière valeur est égale à XIS
+                        ((Button) v).setText(BULE);// si la valeur est égale a X on jeu BULE
+                        setLastPlay(BULE); // set lastPlay comme Bule
+						//pour la prochaine fois il ne entrer pas dans cette condition
                     } else {
-                        ((Button) v).setText(XIS);
-                        setLastPlay(XIS);
+                        ((Button) v).setText(XIS);//Set le text du button comme XIS
+                        setLastPlay(XIS); //set lastPlay comme XIS
                     }
-
+				// message au jouer qu'il devrait choisir une carre vide 
                 } else {
                     Toast.makeText(getView().getContext(), "Il faut choisir autre carre.", Toast.LENGTH_LONG).show();
 
                 }
-                isEnd();
+                isEnd(); // on verifie se est le fin du jeu
 
             }
         }
 
         public Button getCarre(int tagNum){
+			//retorna o respectivo quadrado requerido pela variavel tagNum
             return (Button)getView().findViewWithTag("carre"+tagNum);
         }
-
+		
+		//funçao para iniciar um novo jogo
         public void newGame(View v){
 
             setEnableGame(true);
@@ -87,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
-
+	//habilitar os quadrados para jogar.
     public void setEnableGame(boolean b){
         for (int i = 1; i <=9; i++){
             if(getCarre(i)!=null){
@@ -95,19 +106,18 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
+	//funçao para setar a cor do texto X ou O no jogo
     public void setColorText(int bot, int colorX){
         getCarre(bot).setTextColor(getResources().getColor(colorX));
-
-
-
     }
 
+	// funçao para setar a cor do botao (mudar a cor do botão quando o jogo for ganho)
     public void setColorBot(int bot, int colorX){
         getCarre(bot).setBackgroundColor(getResources().getColor(colorX));
 
     }
-
+	
+	// setar a cor preta do texto depois que terminar o jogo
     public void setColorBlack(){
         for(int j=0; j<=9; ++j){
             if(getCarre(j)!=null){
@@ -115,6 +125,10 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+	// setar a cor original dos botoes depois que terminar o jogo. 
+	// serve voltar a cor normal dos botões quando o jogo for reiniciado 
+	// metodo chamado no outro méthodo newGame( o que inicia as funcoes do jogo voltando a cor do texto para preto) 
 
     public void setColorNormal(){
         for(int j=0; j<=9; ++j){
@@ -125,6 +139,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public boolean isEnd(){
+		
+		// vamos percorrer todas as nosas condições definidas na nossa matriz
 
         for(int x=0; x<=7; ++x){
 
@@ -132,25 +148,30 @@ public class MainActivity extends AppCompatActivity {
             String s2 = getCarre(etatFinal[x][1]).getText().toString();
             String s3 = getCarre(etatFinal[x][2]).getText().toString();
 
+			
+			// verificamos se os botoes sao diferentes de vazio
+			// pois se nao houver verificaçao o jogo acaba assim que iniciado
+			// pois os botoes terao o mesmo conteúdo
             if ((!s1.equals(""))&&
                     (!s2.equals(""))&&
                     (!s3.equals(""))){
 
 
-
+				//verifica a igualdade
                 if(s1.equals(s2)&& s2.equals(s3)){
-                    //change la couleur de X ou de O
+                    //change la couleur de X ou de O (aquele que ganhou o jogo)
                     setColorText(etatFinal[x][0],R.color.red);
                     setColorText(etatFinal[x][1],R.color.red);
                     setColorText(etatFinal[x][2],R.color.red);
+					//muda a cor dos botoes que ganharam o jogo
                     setColorBot(etatFinal[x][0],R.color.magenta);
                     setColorBot(etatFinal[x][1],R.color.magenta);
                     setColorBot(etatFinal[x][2],R.color.magenta);
 
-
+					//indicamos aqui que o jogo acabou
                     Toast.makeText(getView().getContext(),"Fim do jogo!", Toast.LENGTH_LONG).show();
 
-                    return true;
+                    return true; // sim o jogo acabou
                 }
             }
         }return false;
@@ -207,7 +228,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
-
+// menu new game
     private void newGame() {
         setEnableGame(true);
         setColorBlack();
