da9a87194968b2f30693864246e64576adca8373
Marcelo Santos <msantosecnm@hotmail.com>
2017-09-27 00:25:03 -0400

    Alteraçoes
    Add imagens, classe, alteraçao no model xml e na main activity

--- ListViewPersonalizado/app/src/main/java/br/com/darksite/listviewpersonalizado/MainActivity.java
+++ ListViewPersonalizado/app/src/main/java/br/com/darksite/listviewpersonalizado/MainActivity.java
@@ -4,11 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.*;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    ArrayList cidades = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        cidades.add(new Cidade("Uberlândia", "Capital do Triângulo Mineiro", "MG", R.drawable.uberlandia));
+        cidades.add(new Cidade("Curitiba", "Capital do Paraná", "PR", R.drawable.curitiba));
+        cidades.add(new Cidade("São Paulo", "Capital de São Paulo", "SP",R.drawable.sp));
+        cidades.add(new Cidade("Porto Alegre", "Capital do Rio Grande do Sul", "RS", R.drawable.poa));
+
+
+
     }
 }
